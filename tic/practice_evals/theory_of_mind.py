a93af101cb51dd4ce068395acedd5db6114b47bd
altock <6935548+altock@users.noreply.github.com>
2024-06-29 22:46:45 -0700

    Tic System Prompt

--- tic/practice_evals/theory_of_mind.py
+++ tic/practice_evals/theory_of_mind.py
@@ -1,16 +1,18 @@
-from inspect_ai import Task, eval, task
+"""To run this script:
+  inspect eval theory_of_mind.py --model openai/gpt-3.5-turbo --max-connections 20;
+  inspect view
+"""
 
-# TODO: Remove
-from inspect_ai.dataset import example_dataset
+from inspect_ai import Task, eval, task
 from inspect_ai.scorer import includes
-from inspect_ai.solver import chain_of_thought, generate, self_critique
+from inspect_ai.solver import chain_of_thought, generate, self_critique, system_message
 from inspect_ai.dataset import json_dataset
 
 
 @task
 def theory_of_mind():
     return Task(
-        dataset=json_dataset("../input_files/practice_input_file_16.jsonl"),
-        # plan=[chain_of_thought(), generate(), self_critique()],
+        dataset=json_dataset("../input_files/practice_input_file.jsonl"),
+        plan=[system_message("system.txt"), generate()],
         scorer=includes(),
     )
