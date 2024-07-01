01bf5e25b97d3bc9754be13e2854ea8a50e5d791
altock <6935548+altock@users.noreply.github.com>
2024-06-30 19:27:56 -0700

    Eval results

--- tic/lie_of_omission.py
+++ tic/lie_of_omission.py
@@ -8,6 +8,7 @@ from functools import partial
 
 from inspect_ai import Task, eval, task
 from inspect_ai.dataset import json_dataset
+from inspect_ai.log import list_eval_logs
 from inspect_ai.model import get_model
 from inspect_ai.scorer import (
     accuracy,
@@ -171,38 +172,44 @@ claude_generated_lie_system_prompt_v1 = (
 claude_generated_system_prompt_v0 = (
     "tic_system_prompts/claude_generated_system_prompt_v0.txt"
 )
-
-
-def basic_lie_of_omission():
-    return lie_of_omission(
-        dataset_json="",
-        system_prompt_file="",
-        scorerFn=includes(),
-        metrics=[accuracy, bootstrap_std],
-    )
-
-
-eval(
-    lie_of_omission(
-        dataset_json=gpt4_generated_outright_lie_dataset_json,
-        system_prompt_file=claude_generated_lie_system_prompt_v1,
-        scorerFn=includesOrNot(),
-        metrics=[accuracy, bootstrap_std, precision, recall],
-    ),
-    model="anthropic/claude-3-5-sonnet-20240620",
-    max_connections=20,
+openai_generated_lie_system_prompt_v0 = (
+    "tic_system_prompts/openai_generated_lie_system_prompt_v0.txt"
 )
+
+
+# def basic_lie_of_omission():
+#     return lie_of_omission(
+#         dataset_json="",
+#         system_prompt_file="",
+#         scorerFn=includes(),
+#         metrics=[accuracy, bootstrap_std],
+#     )
+
+
+# eval(
+#     lie_of_omission(
+#         dataset_json=gpt4_generated_outright_lie_dataset_json,
+#         system_prompt_file=claude_generated_lie_system_prompt_v1,
+#         scorerFn=includesOrNot(),
+#         metrics=[accuracy, bootstrap_std, precision, recall],
+#     ),
+#     model="anthropic/claude-3-5-sonnet-20240620",
+#     max_connections=20,
+# )
+# %%
+print(list_eval_logs()[0])
+
 
 # %%
 
 # TODO: Run async on all these models eventually
 # https://ukgovernmentbeis.github.io/inspect_ai/eval-tuning.html#model-requests
 
-models = [
-    get_model("openai/gpt-3.5-turbo"),
-    get_model("anthropic/claude-2.1"),
-    #   get_model("mistral/mistral-large-latest")
-]
+# models = [
+#     get_model("openai/gpt-3.5-turbo"),
+#     get_model("anthropic/claude-2.1"),
+#     #   get_model("mistral/mistral-large-latest")
+# ]
 
 # output = "Output to be scored"
 # prompt = f"Could you please score the following output?\n\n{output}"
@@ -211,4 +218,81 @@ models = [
 
 # grader_outputs = await asyncio.gather(*graders)
 
+# %%
+models = [
+    "anthropic/claude-3-5-sonnet-20240620",
+    "anthropic/claude-3-haiku-20240307",
+    "anthropic/claude-3-sonnet-20240229",
+    # "anthropic/claude-3-opus-20240229",
+    "openai/gpt-3.5-turbo",
+    "openai/gpt-4o",
+    "openai/gpt-4",
+    # "openai/ft:gpt-3.5-turbo-0125:deceptiondetection:emojiticv0:9fgvspqj",
+]
+for model in models:
+    eval(
+        lie_of_omission(
+            dataset_json=gpt4_generated_outright_lie_dataset_json,
+            system_prompt_file=openai_generated_lie_system_prompt_v0,
+            scorerFn=includesOrNot(),
+            metrics=[accuracy, bootstrap_std, precision, recall],
+        ),
+        model=model,
+        max_connections=20,
+    )
+    eval(
+        lie_of_omission(
+            dataset_json=gpt4_generated_outright_lie_dataset_json,
+            system_prompt_file=claude_generated_lie_system_prompt_v1,
+            scorerFn=includesOrNot(),
+            metrics=[accuracy, bootstrap_std, precision, recall],
+        ),
+        model=model,
+        max_connections=20,
+    )
+
+# %%
+import json, os
+import pandas as pd
+
+
+def process_log_file(file_path: str, model_gen_system_prompt:str) -> dict:
+    with open(file_path, "r") as f:
+        log_json = json.load(f)
+
+    results = log_json.get("results", {})
+    metrics = results.get("metrics", {})
+
+    return {
+        "model": log_json.get("eval", {}).get("model", ""),
+        "model_that_generated_system_prompt": model_gen_system_prompt,
+        "accuracy": metrics.get("accuracy", {}).get("value", None),
+        "precision": metrics.get("precision", {}).get("value", None),
+        "recall": metrics.get("recall", {}).get("value", None),
+        "bootstrap_std": metrics.get("bootstrap_std", {}).get("value", None),
+    }
+
+
+def main():
+    log_files = [
+        "/".join(log.name.split("/")[-2:]) for log in list_eval_logs()[: len(models)*2]
+    ]
+    data = []
+    isChatgpt = True
+    for filename in log_files:
+        # file_path = os.path.join(directory, filename)
+        data.append(process_log_file(filename, "chatgpt" if isChatgpt else "claude"))
+        isChatgpt = not isChatgpt
+
+    df = pd.DataFrame(
+        data, columns=["model", "model_that_generated_system_prompt","accuracy", "precision", "recall", "bootstrap_std"]
+    )
+    return df
+
+
+result_df_openai = main()
+print(result_df_openai)
+
+# %%
+result_df_openai.to_csv("results.csv")
 # %%
